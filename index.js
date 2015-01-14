412fc069004cfca1ed0a842a7b319c708490cf9d
Maik Hummel <yo@beevelop.com>
2015-01-14 09:59:09 +0100

    fix autoloader

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+var path = require('path');
+var subfolder = path.resolve(__dirname, 'lib/');
+
 module.exports = {};
-require('fs').readdirSync(__dirname + '/lib/').forEach(function(file) {
+require('fs').readdirSync(subfolder).forEach(function(file) {
     if (file.match(/.+\.js/g) !== null && file !== 'index.js') {
         var name = file.replace('.js', '');
-        module.exports[name] = require('./' + file);
+        var lib = path.resolve(subfolder, file);
+        module.exports[name] = require(lib);
     }
 });
