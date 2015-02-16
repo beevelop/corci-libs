92ba7350b55164f0092f518163ce92b2e66bdfd9
Maik Hummel <yo@beevelop.com>
2015-02-16 10:11:13 +0100

    include folders via index.js

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
-var path = require('path');
-var subfolder = path.resolve(__dirname, 'lib/');
-
-module.exports = {};
-require('fs').readdirSync(subfolder).forEach(function(file) {
-    if (file.match(/.+\.js/g) !== null && file !== 'index.js') {
-        var name = file.replace('.js', '');
-        var lib = path.resolve(subfolder, file);
-        module.exports[name] = require(lib);
-    }
-});
+module.exports = require('./lib');
+module.exports.models = require('./models');
---
+++ models/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    BuildRequest: require('./BuildRequest'),
+    BuildRequestStatus: require('./BuildRequestStatus'),
+    Build: require('./Build'),
+    BuildStatus: require('./BuildStatus'),
+    BuildFile: require('./BuildFile'),
+    BuildQueue: require('./BuildQueue')
+};
